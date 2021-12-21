594097bfd22422920960941fd58f6a3af6aca395
ehsan bazaei <bazaeiehsan@gmail.com>
2021-12-21 09:17:11 +0100

    transfer-1

---
+++ script.js
@@ -0,0 +1,23 @@
+// const test = "salam";
+// console.log(test.substring(1,3).toUpperCase())
+
+// const fruit1 = 'salam';
+// const fruit = ['a1','a2','a3'];
+// console.log(fruit[1]);
+// fruit[3] = 'a4';
+// fruit.push('a5');
+// console.log(fruit);
+// fruit[3] = 'a0';
+
+// console.log(fruit);
+// fruit.pop();
+// console.log(fruit);
+// console.log(Array.isArray(fruit))
+
+// num = 1;
+// for(let i=1; i <= 99 ; i=i+2){
+//    console.log( num ,i) ;
+//    ++num
+// }
+// console.log(num)
+
